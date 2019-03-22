edbfe5a4e2d6d1144d6236cf9e18e20fab9cc488
DmitriyShadziun <m_e_x_a_n_i_k@tut.by>
2019-03-22 17:37:35 +0300

    test complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if (str.trim().length===0 || bracketsConfig.length===0) return false;
+    var stek = [];
+    var first = {};
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i].length!==2) {
+            return false;
+        }
+    }
+
+    const arrStr = str.split('');
+    for (var s = 0; s < arrStr.length; s++) {
+        for (var i = 0; i < bracketsConfig.length; i++) {
+            if (arrStr[s]===bracketsConfig[i][0]) {
+                if (first[bracketsConfig[i][0]] == null) {
+                    stek.push(bracketsConfig[i][0]);
+                    if (bracketsConfig[i][0]===bracketsConfig[i][1]) { first[bracketsConfig[i][0]] = true; }
+                } else {
+                    delete first[bracketsConfig[i][0]];
+                }
+
+            }
+            if (arrStr[s]===bracketsConfig[i][1]) {
+                if (first[bracketsConfig[i][0]] == null) {
+                    if (stek.length===0) return false;
+                    var p = stek.pop();
+                    if (p !== bracketsConfig[i][0]) return false;
+                }
+            }
+        }
+    }
+
+    return stek.length===0;
 }
